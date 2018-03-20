dcb4755e9ebe3d3894796a0cc376b91b60a3e712
javig <javig@LAPTOP-LNNQNAND>
2018-03-20 18:26:07 +0100

    Añadidas clases UsuarioDao y UsuarioPOJO

---
+++ src/main/java/es/ucm/fdi/integracion/UsuarioPOJO.java
@@ -0,0 +1,84 @@
+package es.ucm.fdi.integracion;
+
+/**
+ * 
+ * @author Javier Guzmán
+ *
+ */
+
+public class UsuarioPOJO {
+	private String idUsuario;
+	private String nombreReal;
+	private int puntuacion;
+	private String descPerfil;
+	private String password;
+	private String country;
+	
+	public UsuarioPOJO(String idUsuario, String nombreReal, int puntuacion,
+			String descPerfil, String password, String country) {
+		this.idUsuario = idUsuario;
+		this.nombreReal = nombreReal;
+		this.puntuacion = puntuacion;
+		this.descPerfil = descPerfil;
+		this.password = password;
+		this.country = country;
+	}
+	
+	public String getIdUsuario() {
+		return idUsuario;
+	}
+
+	public void setIdUsuario(String idUsuario) {
+		this.idUsuario = idUsuario;
+	}
+
+	public String getNombreReal() {
+		return nombreReal;
+	}
+
+	public void setNombreReal(String nombreReal) {
+		this.nombreReal = nombreReal;
+	}
+
+	public int getPuntuacion() {
+		return puntuacion;
+	}
+
+	public void setPuntuacion(int puntuacion) {
+		this.puntuacion = puntuacion;
+	}
+
+	public String getDescPerfil() {
+		return descPerfil;
+	}
+
+
+	public void setDescPerfil(String descPerfil) {
+		this.descPerfil = descPerfil;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	@Override
+	public String toString() {
+			return idUsuario + '\n' + descPerfil + '\n'+ "Puntuacion: " + puntuacion;
+	
+	}
+	
+	
+	
+}
