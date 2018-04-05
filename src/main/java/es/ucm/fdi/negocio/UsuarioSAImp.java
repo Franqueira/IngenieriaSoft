766a61a2a123e5ab3c20fc40a8a0d92f593d21f3
franqui <migfranq@ucm.es>
2018-04-05 20:31:06 +0200

    Ultimos cambios Usuario,Clan

--- src/main/java/es/ucm/fdi/negocio/UsuarioSAImp.java
+++ src/main/java/es/ucm/fdi/negocio/UsuarioSAImp.java
@@ -3,16 +3,23 @@
 package es.ucm.fdi.negocio;
 
 import es.ucm.fdi.integracion.DAOs.AlarmaUsuarioDAOImp;
+import es.ucm.fdi.integracion.DAOs.ClanDAOImp;
 import es.ucm.fdi.integracion.DAOs.UsuarioClanDAOImp;
 import es.ucm.fdi.integracion.DAOs.UsuarioDAOImp;
+import es.ucm.fdi.integracion.POJOs.ClanPOJO;
 import es.ucm.fdi.integracion.POJOs.UsuarioPOJO;
 
 public class UsuarioSAImp implements UsuarioSA {
 	public UsuarioDAOImp usuario;
 	public UsuarioClanDAOImp usuarioClan;
 	public AlarmaUsuarioDAOImp usuarioAlarma;
+	public ClanDAOImp clanes;
 	public void EliminarUsuarioClan(String idUsuario) {
-	
+	String idClan=usuario.getUsuario(idUsuario).getIdClan();
+	usuarioClan.eliminaUsuarioClan(idUsuario);
+		if(clanes.getClan(idClan).getLider().equals(idUsuario)){
+			clanes.getClan(idClan).cam;
+		}
 	}
 
 	public void AnadirUsuarioClan(String idUsuario,String idClan) {
