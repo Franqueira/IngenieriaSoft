327a6e531b26ec5fe3c41f5ebb18500029cd5c92
javig <javig@LAPTOP-LNNQNAND.mshome.net>
2018-04-05 18:51:34 +0200

    AlarmaUsuarioDAO creado

---
+++ src/main/java/es/ucm/fdi/integracion/DAOs/AlarmaUsuarioDAO.java
@@ -0,0 +1,20 @@
+package es.ucm.fdi.integracion.DAOs;
+
+import java.util.ArrayList;
+
+import es.ucm.fdi.integracion.POJOs.AlarmaPOJO;
+import es.ucm.fdi.integracion.POJOs.AlarmaUsuarioPOJO;
+
+/**
+ * 
+ * @author Javier Guzmán
+ *
+ */
+public interface AlarmaUsuarioDAO {
+	public ArrayList<String> getAlarmasUsuario(String idUsuario);
+	
+	public void removeAlarmaUsuario(String idAlarma);
+	
+	public void addAlarmaUsuario(AlarmaUsuarioPOJO au);
+	
+}
