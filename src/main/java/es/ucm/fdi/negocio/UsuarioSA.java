70a2b65d6671a54e04536a38c61be6582bfcccfa
franqui <migfranq@ucm.es>
2018-04-05 18:15:52 +0200

    Creacion UsuarioSA y corrección de AlarmaSA

---
+++ src/main/java/es/ucm/fdi/negocio/UsuarioSA.java
@@ -0,0 +1,14 @@
+package es.ucm.fdi.negocio;
+
+public interface UsuarioSA {
+
+	public boolean EliminarUsuarioClan(String idUsuario);
+
+	public boolean AnadirUsuarioClan(String idUsuario);
+
+	public void AnadirAlarma(String idAlarma, int horas, int minutos,
+			boolean active, String Tono);
+
+	public void EliminarAlarma(String idAlarma);
+
+}
