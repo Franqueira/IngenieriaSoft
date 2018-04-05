ab8938a8d0558f0e69d89e89661b7e712bcf9c1a
javig <javig@LAPTOP-LNNQNAND.mshome.net>
2018-04-05 19:55:11 +0200

    19:55

---
+++ src/main/java/es/ucm/fdi/negocio/TestperclanSA.java
@@ -0,0 +1,9 @@
+package es.ucm.fdi.negocio;
+
+import java.util.ArrayList;
+
+import es.ucm.fdi.integracion.POJOs.UsuarioPOJO;
+
+public interface TestperclanSA {
+	public ArrayList<UsuarioPOJO> getRanking(String nombreClan);
+}
