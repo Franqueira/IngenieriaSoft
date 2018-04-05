f976c23a3aedbebad5d0b15a7903b96ed0fb00a3
jaimefd <skyjimmy7@gmail.com>
2018-04-05 18:49:16 +0200

    Fixed UsuarioClanDAO and UsuarioAmigoDAO

--- src/main/java/es/ucm/fdi/integracion/DAOs/UsuarioClanDAOImp.java
+++ src/main/java/es/ucm/fdi/integracion/DAOs/UsuarioClanDAOImp.java
@@ -2,42 +2,15 @@ package es.ucm.fdi.integracion.DAOs;
 
 import java.util.ArrayList;
 
-import es.ucm.fdi.datos.BD;
 import es.ucm.fdi.integracion.POJOs.UsuarioClanPOJO;
 
-/**
- * 
- * @author jaimefd
- *
- */
-
-public class UsuarioClanDAOImp {
-	private BD<UsuarioClanPOJO> miembros; 
-	// el ID de cada UsuarioClanPOJO en la BD es el ID del usuario, ya que solo puede tener 1 clan
-
-	public UsuarioClanDAOImp(BD<UsuarioClanPOJO> miembros) {
-		super();
-		this.miembros = miembros;
-	}
+public interface UsuarioClanDAOImp {
 	
-	public String getClan(String id){
-		return miembros.find(id).getIdClan();
-	}
+	public String getClan(String id);
 	
-	public ArrayList<String> getMiembrosClan(String clan){
-		ArrayList<String> miembrosClan =  new ArrayList<String>();
-		for(String id : miembros.getIds()){
-			if(getClan(id).equals(clan))
-				miembrosClan.add(id);
-		}
-		return miembrosClan;
-	}
+	public ArrayList<String> getMiembrosClan(String clan);
 	
-	public void guardaUsuarioClan(UsuarioClanPOJO uclan){
-		miembros.insert(uclan, uclan.getIdUsuario());
-	}
+	public void guardaUsuarioClan(UsuarioClanPOJO uclan);
 	
-	public void eliminaUsuarioClan(UsuarioClanPOJO uclan){
-		miembros.removeId(uclan.getIdUsuario());
-	}
+	public void eliminaUsuarioClan(UsuarioClanPOJO uclan);
 }
