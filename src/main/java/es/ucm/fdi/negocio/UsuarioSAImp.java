b880b0f0d1edc8e5a52c3381c8dccb9c1287ab45
franqui <migfranq@ucm.es>
2018-04-05 18:48:34 +0200

    Pequeñas modificaciones

--- src/main/java/es/ucm/fdi/negocio/UsuarioSAImp.java
+++ src/main/java/es/ucm/fdi/negocio/UsuarioSAImp.java
@@ -3,32 +3,34 @@ package es.ucm.fdi.negocio;
 import es.ucm.fdi.integracion.DAOs.UsuarioDAOImp;
 import es.ucm.fdi.integracion.POJOs.UsuarioPOJO;
 
-public class UsuarioSAImp implements UsuarioSA{
+public class UsuarioSAImp implements UsuarioSA {
 	public UsuarioDAOImp usuario;
-	
-	@Override
+
 	public boolean EliminarUsuarioClan(String idUsuario) {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
-	@Override
 	public boolean AnadirUsuarioClan(String idUsuario) {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
-	@Override
+	public boolean AnadirUsuario(String idUsuario, String nombreReal,
+			int puntuacion, String descPerfil, String password, String country) {
+
+		return false;
+	}
+
 	public void AnadirAlarma(String idAlarma, int horas, int minutos,
 			boolean active, String Tono) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
-	@Override
 	public void EliminarAlarma(String idAlarma) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
