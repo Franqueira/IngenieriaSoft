52e3f9c004f08df415168dcbb4898306e91008b2
jaimefd <skyjimmy7@gmail.com>
2018-04-06 18:33:35 +0200

    UsuarioClanDAOTest (en proceso)

---
+++ src/test/java/es/ucm/fdi/integracion/DAOs/UsuarioClanDAOTest.java
@@ -0,0 +1,11 @@
+package es.ucm.fdi.integracion.DAOs;
+
+import junit.framework.TestCase;
+
+public class UsuarioClanDAOTest extends TestCase {
+	
+	public UsuarioClanDAOTest(String testName){
+		super(testName);
+	}
+
+}
