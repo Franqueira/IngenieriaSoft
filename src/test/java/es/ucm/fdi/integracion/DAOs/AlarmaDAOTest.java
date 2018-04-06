2d61932208370751e2927fef0f69b7d010c3f4b2
franqui <migfranq@ucm.es>
2018-04-06 13:30:13 +0200

    Creado base de los Test

---
+++ src/test/java/es/ucm/fdi/integracion/DAOs/AlarmaDAOTest.java
@@ -0,0 +1,28 @@
+package es.ucm.fdi.integracion.DAOs;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+import es.ucm.fdi.integracion.POJOs.AlarmaPOJO;
+
+public class AlarmaDAOTest {
+	
+	@Test
+	public void getAlarmaTest(){
+	
+	}
+	
+	@Test
+	public void saveAlarmTest(){
+		
+	}
+	@Test
+	public void removeAlarmTest(){
+		
+	}
+	@Test
+	public void getActiveTest(){
+		
+	}
+}
---
+++ src/test/java/es/ucm/fdi/integracion/DAOs/UsuarioDAOTest.java
@@ -0,0 +1,28 @@
+package es.ucm.fdi.integracion.DAOs;
+
+import org.junit.Test;
+
+public class UsuarioDAOTest {
+	@Test
+	public  void getUsuarioTest(){
+	
+	
+	}
+	@Test
+	public void guardaUsuarioTest(){
+		
+	}
+	@Test 
+	public void eliminaUsuarioTest(){
+		
+	}
+	@Test
+	public void findTest(){
+		
+		
+	}
+	@Test
+	public void getUsersByCountryTest(){
+		
+	}
+}
